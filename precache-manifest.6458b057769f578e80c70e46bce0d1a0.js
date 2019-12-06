35465dbe07554fa57287e2c8f31606a1a3d6f077
Adam Siwiec <adam.siwiec@jbhunt.com>
2019-12-05 23:30:45 -0800

    Updates

---
+++ precache-manifest.6458b057769f578e80c70e46bce0d1a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae7f9562da6500927a40b41ddf8912ea",
+    "url": "/jerrycoin/index.html"
+  },
+  {
+    "revision": "64d66089e00b2ddb9ab5",
+    "url": "/jerrycoin/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3e9dd4cd0511f7a053d1",
+    "url": "/jerrycoin/static/js/2.d7a86fa1.chunk.js"
+  },
+  {
+    "revision": "ee5cb889d4ebfd76f0a2922109b339bc",
+    "url": "/jerrycoin/static/js/2.d7a86fa1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "64d66089e00b2ddb9ab5",
+    "url": "/jerrycoin/static/js/main.33a0d6ae.chunk.js"
+  },
+  {
+    "revision": "d399cb83b011b2c7e2be",
+    "url": "/jerrycoin/static/js/runtime-main.36bc6fdb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jerrycoin/static/media/logo.5d5d9eef.svg"
+  }
+]);
