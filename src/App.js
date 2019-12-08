62c5b8956b9e745a65c979f3988700976bb4f05d
Adam Siwiec <adam.siwiec@jbhunt.com>
2019-12-08 13:53:17 -0800

    big ui fixes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-// import "assets/vendor/nucleo/css/nucleo.css";
-// import "assets/vendor/font-awesome/css/font-awesome.min.css";
-// import "assets/scss/argon-design-system-react.scss";
-
 import React, {useState, useEffect, useCallback} from 'react';
-import io from 'socket.io-client';
 import logo from './logo.svg';
 import './App.css';
 import {generatePrivateKey, getPublicFromWallet} from './crypto/wallet';
-import {sendTransaction, generateNextBlock, getAccountBalance, getBlockchain, setBlockchain, replaceChain} from "./crypto/blockchain"
+import {sendTransaction, generateNextBlock, getAccountBalance, getBlockchain} from "./crypto/blockchain"
 import { useCookies } from 'react-cookie';
 import { getTransactionPool, setTransactionPool } from "./crypto/transactionPool"
 import {
@@ -25,9 +20,11 @@ import {
   Form,
   FormGroup,
   Input,
+  Label,
   InputGroupAddon,
   InputGroupText,
   InputGroup,
+  Jumbotron,
   Container,
   Row,
   Col
@@ -37,16 +34,16 @@ import useSocket from "use-socket.io-client";
 import { useAlert } from "react-alert"
 import QRCode from "qrcode.react";
 
-    // const socket = io('https://jerry-server.herokuapp.com', {
-    //   transports: ['websocket']
-    // });
+ 
 
 
 function App() {
-    const [socket] = useSocket("https://jerry-server.herokuapp.com", {
-       transports: ['websocket']
-     });
-    const [data, setData] = useState(0);
+     const [socket] = useSocket("https://jerry-server.herokuapp.com", {
+    //const [socket] = useSocket("http://localhost:4001", {
+
+    transports: ['websocket']
+
+      });
     const [first, setFirst] = useState(true);
     const [cookies, setCookie, removeCookie] = useCookies(['jerry']);
     const [privateKey, setPrivateKey] = useState("");
@@ -54,7 +51,10 @@ function App() {
     const [txPool, setTxPool] = useState(getTransactionPool());
     const [address, setAddress] = useState("");
     const [amount, setAmount] = useState(0);
-    const [bc, setBc] = useState(getBlockchain());
+    const [username, setUsername] = useState("");
+    const [tempUser, setTempUser] = useState("");
+    const [users, setUsers] = useState([]);
+    const [death, setDeath] = useState(true);
     const alert = useAlert();
 
 
@@ -83,13 +83,25 @@ function App() {
 
   // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
-        console.log("yeah", cookies.jerry)
-        //Very simply connect to the socket
+ if (cookies.jerry == undefined) {
+   let key = generatePrivateKey();
+   console.log("making key", key)
+   setCookie("jerry", key);
+
+   setPrivateKey(getPublicFromWallet());
+ } else {
+   console.log(cookies.jerry);
+   setPrivateKey(getPublicFromWallet());
+ } //Very simply connect to the socket
         //Listen for data on the "outgoing data" namespace and supply a callback for what to do when we get one. In this case, we set a state variable
-        if (first)  {
+        if (first && username != "")  {
           console.log("running first");
           socket.emit("getTxPool");
           socket.emit("getBlockchain");
+          socket.emit("setUser", {
+            username: username,
+            publicKey: privateKey
+          })
           setFirst(false);
            socket.on("resTxPool", TxPool => {
              console.log("got pool:", TxPool);
@@ -108,44 +120,100 @@ function App() {
              console.log("got bc:", bc);
              refreshBlock(bc);
            });
+           socket.on("users", users => {
+                         console.log(users);
+ 
+            users = users.filter(function (obj) {
+               return obj.username !== username;
+             });
+            alert.show("New User Joined");
+             console.log(users);
+             setUsers(users);
+           })
+            socket.on("userLeft", users => {
+              console.log(users);
+
+              users.users = users.users.filter(function (obj) {
+                return obj.username !== username;
+              });
+              alert.show("user " + users.left + " left");
+              console.log(users.users);
+              setUsers(users.users);
+            })
         }
        
 
 
-        if(cookies.jerry == undefined) {
-          let key = generatePrivateKey();
-                    console.log("making key", key)
-                    setCookie("jerry", key);
-
-          setPrivateKey(getPublicFromWallet());
-        } else {
-          console.log(cookies.jerry);
-          setPrivateKey(getPublicFromWallet());
-        }
-        return () => {
-          socket.emit("disconnect");
-        }
-  }, [cookies, privateKey, first, setBalance, setPrivateKey, removeCookie, setCookie, refreshBlock, refreshTxPool, socket]);
+       
+        
+  }, [cookies, privateKey, first, setBalance, setPrivateKey, removeCookie, setCookie, refreshBlock, refreshTxPool, socket, username, alert, death]);
 
 
   return (
     <>
-    <img src={require('./assets/ultimateJerry.gif')}/>
-      <h1> Jerry Coin </h1>
-      {/* <p>{data}</p> */}
-      <Form>
-        <Row>
-          <Col>
-            <Input
+<Container>
+
+<Card> 
+<CardImg src={require('./assets/ultimateJerry.gif')}/>
+<CardBody>
+<h1 className = "display-3" > Jerry Coin </h1> 
+<p className = "lead" > This is Jerry Coin, a decentralized, consensus-based cryptocurrency, based right here on the web. </p> 
+</CardBody>
+</Card>    
+
+<hr className = "my-2" / >
+    
+    
+      
+     {username == "" &&
+     <>
+      <InputGroup>
+       < Input
+       type = "text"
+       
+       onChange = {
+         e => {
+          setTempUser(e.target.value)
+         }
+       }
+       />
+      <InputGroupAddon>
+      <Button onClick={() => {
+         setUsername(tempUser);
+       }}>Set Username</Button>
+      </InputGroupAddon>
+      </InputGroup>
+      </>}
+      
+       
+       {username != "" &&
+       <>
+          <Label>
+            Send Address
+          </Label>
+
+
+          <InputGroup>
+          <InputGroupAddon addonType="prepend">
+          <InputGroupText>@</InputGroupText>
+        </InputGroupAddon>
+          <Input
               type="text"
               value={address}
+              placeholder="04234adfasdfae9342..."
               onChange={e => setAddress(e.target.value)}
             />
-          </Col>
-
-          <Col>
-            <Input
-              type="text"
+          </InputGroup>
+          
+          <br></br>
+          
+         <InputGroup>
+         <InputGroupAddon>
+         <InputGroupText>
+         ₵
+         </InputGroupText>
+         </InputGroupAddon>
+          <Input
               value={amount}
               onChange={e => {
                 let amount;
@@ -157,12 +225,26 @@ function App() {
                 setAmount(amount);
               }}
             />
-          </Col>
-        </Row>
-      </Form>
+        <InputGroupAddon addonType = "append" >
+        <Button
+        onClick={() => {
+          socket.emit("sendTx", sendTransaction(address, amount));
+        }}
+      >
+      Send JC
+      </Button>
+        </InputGroupAddon>  
+         </InputGroup>
+           
 
-      <QRCode value={privateKey} />
 
+      
+<Row>
+  <Col className="my-3" md="6">
+ <QRCode value={privateKey} />
+
+  </Col>
+  < Col className = "my-3" md="6">
       <Button
         onClick={() => {
           generateNextBlock();
@@ -172,21 +254,26 @@ function App() {
       >
         Generate Block
       </Button>
+</Col>
+</Row>
+     
 
-      <Button
-        onClick={() => {
-          socket.emit("sendTx", sendTransaction(address, amount));
-        }}
-      >
-        Send Transaction
-      </Button>
 
-      {txPool.forEach(item => {
-        return <p>item.id</p>;
-      })}
+       <ul> {
+           users.map(user => {
+            return ( <Card>
+              <p> { user.username } </p> 
+              </Card>)
+           }) 
+           } 
+         </ul>
 
       <p> {privateKey} </p>
       <p> {balance} </p>
+      </>
+          }
+      </Container>
+
     </>
   );
 }
--- src/views/IndexSections/Hero.jsx
+++ src/views/IndexSections/Hero.jsx
@@ -31,10 +31,7 @@ class Hero extends React.Component {
           <section className="section section-lg section-hero section-shaped">
             {/* Background circles */}
             <div className="shape shape-style-1 shape-default">
-              <span className="span-150" />
-              <span className="span-50" />
-              <span className="span-50" />
-              <span className="span-75" />
+              
               <span className="span-100" />
               <span className="span-100" />
               <span className="span-100" />
@@ -49,10 +46,10 @@ class Hero extends React.Component {
               <span className="span-50" />
               <span className="span-100" />
             </div>
-            <Container className="shape-container d-flex align-items-center py-lg">
+            <Container className="shape-container d-flex align-items-center ">
               <div className="col px-0">
                 <Row className="align-items-center justify-content-center">
-                  <Col className="text-center" lg="6">
+                  <Col className="text-center" lg="8">
 
                     <App />
 
@@ -60,22 +57,7 @@ class Hero extends React.Component {
                 </Row>
               </div>
             </Container>
-            {/* SVG separator */}
-            {/* <div className="separator separator-bottom separator-skew zindex-100">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                preserveAspectRatio="none"
-                version="1.1"
-                viewBox="0 0 2560 100"
-                x="0"
-                y="0"
-              >
-                <polygon
-                  className="fill-white"
-                  points="2560 0 2560 100 0 100"
-                />
-              </svg>
-            </div> */}
+          
           </section>
         </div>
       </>
